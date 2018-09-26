90060eaa85a6411b6b9aa87a95f4e6b28828eb8a
Rachel Fuller <rlfuller@gmail.com>
2018-09-25 22:33:20 -0400

    pseudo code planning the click function

--- js/main.js
+++ js/main.js
@@ -60,6 +60,75 @@ var fragment = document.createDocumentFragment();
   //let shuffledArray = shuffle(matchingClasses);
 
   let successIcon = "far fa-grin";
+  let firstClickForMatch = false;
+  let secondClickForMatch = false;
+  let firstCardClass = "";
+  let secondCardClass = "";
+  let firstEl = null;
+  let secondEl = null;
+  
+
+  function clickForMatch(el){ /* pass the element on each click so we can deal with the event listeners */
+    if (firstEl === null){
+      firstEl  = el; 
+      secondEl === null;
+      // firstCardClass = el.classList.get the class out of the classlist that starts with fa-
+      // secondCardClass = "";
+      // firstEl = el;
+      // secondEl = null;
+    } else {
+      secondEl = el;
+      // secondClickForMatch === true; /*may not need this */
+      // firstClickForMatch === false;
+      // secondCardClass = el.classList.get the class out of the classList that starts wtih fa-
+      //if (secondEl.querySelector("i").classList.)
+      //get the firstCard icon
+      let firstClasses = firstEl.querySelector("i").classList;
+      let secondClasses = secondEl.querySelector("i").classList;
+      let firstIcon = firstClasses.forEach(function(firstClass){
+        if(firstClass.startsWith("fa-")){
+          return firstClass;
+        }
+      });
+      let secondIcon = secondClasses.forEach(function(secondClass){
+        if(secondClass.startsWith("fa-")){
+          return secondClass;
+        }
+      });
+
+      if (firstIcon === secondIcon){
+        //if here, this is a match
+        //1)remove the event listeners from the first and second el
+        //2) change the curstor from a pointer to a non-pointer
+        //2) some sort of animation and maybe a message
+        //4) update some sort of total matches
+      } else {
+        //if here, this is not a match
+        //flip the cards back over, show the back
+        
+      }
+      //update the total moves and check to see if we need to change the status / progress /take a star away
+      // if (secondCardClass === firstCardClass){       
+      //   that's a match so 
+      //   1) some sort of transition where it's a match
+      //   2) disable the cursor and remove clicks as these can't be matched anymore or clicked anymore
+      //   3) remove the event listener from both of these --- but how to remove the event
+      //     listener from the first card as now we are on the second card
+       
+      // } else {
+      //   that's not a match, so we need to 
+      //   1) turn both cards bach over
+      //   how do we do this as we are now on the second card
+      // }
+      // then {
+      //   4) update some sort of counter about matches found or matches left
+      //   5) update some sort of counter about how many turns on each second click to set progress / status
+      // }
+
+      firstEl = null;
+
+    }
+  };
 
   function setUpBoard(){
     let shuffledArray = shuffle(matchingClasses);
@@ -79,9 +148,8 @@ var fragment = document.createDocumentFragment();
       //   backOfCard.className = "far fa-meh";
       // });
 
-      div.addEventListener("click", function(){
-        backOfCard.classList.add("unmatched");
-        frontOfCard.classList.remove("unmatched");
+      div.addEventListener("click", function(e){
+        clickForMatch(e.target);
       })
       div.appendChild(backOfCard);
       div.appendChild(frontOfCard);
